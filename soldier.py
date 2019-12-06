04e55d71c85e96192b5c009d1a2c5fb7bc344ffb
LevonMuradyan <levon.muradyan9559@gmail.com>
2019-12-06 23:43:31 +0400

    Soldier v1

--- soldier.py
+++ soldier.py
@@ -6,6 +6,41 @@ import random
 
 # Using enum class create enumerations
 
+class Color(enum.Enum):
+    RED = 1
+    GREEN = 2
+    BLUE = 3
+    PURPLE = 4
+
+class Country(enum.Enum):
+    ENGLAND = 1
+    FRANCE = 2
+    ARMENIA = 3
+
+class Direction(enum.Enum):
+    EAST = 1
+    WEST = 2
+    NORTH = 3
+    SOUTH = 4
+    EASTNORTH = 5
+    EASTSOUTH = 6
+    WESTSOUTH = 7
+    WESTNORTH = 8
+
+def getRandomDirection():
+    rand = random.randint(1,8)
+    switch = {
+        1:Direction.EAST,
+        2:Direction.WEST,
+        3:Direction.NORTH,
+        4:Direction.SOUTH,
+        5:Direction.EASTNORTH,
+        6:Direction.EASTSOUTH,
+        7:Direction.WESTSOUTH,
+        8:Direction.WESTNORTH
+    }
+    return switch.get(rand,0)
+
 class Player:
 
     '''Class Player
@@ -20,18 +55,58 @@ class Player:
                 void __init__(str, str, int, Country, Color)
     '''
 
-    __id
-    __name
-    __rating
-    __country
-    __color
+    __id = ""
+    __name = ""
+    __rating = 0
+    __country = Country.ENGLAND
+    __color = Color.RED
 
-    def __init__(id, name, rating, country, color):
+    def __init__(self, id, name, rating, country, color):
         self.__id = id
         self.__name = name
         self.__rating = rating
         self.__country = country
         self.__color = color
+
+    def getId(self):
+        return self.__id
+
+    def getName(self):
+        return self.__name
+
+    def getRating(self):
+        return self.__rating    
+    
+    def getCountry(self):
+        return self.__country
+
+    def getColor(self):
+        return self.__color
+
+
+
+    def setId(self, id):
+        self.__id = id
+
+    def setName(self, name):
+        self.__name = name
+
+    def setRating(self, rating):
+        self.__rating = rating
+    
+    def setCountry(self, country):
+        self.__country = country
+
+    def setColor(self, color):
+        self.__color = color
+
+    def printPlayer(self):
+        print "\nPlayer settings:"
+        print "\nId =", self.getId() 
+        print "Name =", self.getName() 
+        print "Rating =", self.getRating() 
+        print "Country =", self.getCountry() 
+        print "Color =",self.getColor()
 
 class Location:
     
@@ -41,6 +116,12 @@ class Location:
     def __init__(self, x, y):
         self.__x = x
         self.__y = y
+
+    def __str__(self):
+        return "x = " + str(self.__x) + " y = " + str(self.__y)
+    
+    def __repr__(self):
+        return { 'x':self.__x, 'y':self.__y }
     
     def copyLocation(self, location):
         self.__x = location.__x
@@ -61,42 +142,6 @@ class Location:
     def distanceLocation(self, location):
         return math.sqrt((self.__x - location.__x)**2 + \
             (self.__y - location.__y)**2)
-
-class Direction(enum.Enum):
-    EAST = 1
-    WEST = 2
-    NORTH = 3
-    SOUTH = 4
-    EASTNORTH = 5
-    EASTSOUTH = 6
-    WESTSOUTH = 7
-    WESTNORTH = 8
-    def getRandomDirection():
-        rand = random.randint(1,8)
-        switch = {
-            1:Direction.EAST,
-	        2:Direction.WEST,
-	        3:Direction.NORTH,
-	        4:Direction.SOUTH,
-	        5:Direction.EASTNORTH,
-	        6:Direction.EASTSOUTH,
-	        7:Direction.WESTSOUTH,
-	        8:Direction.WESTNORTH
-        }
-        return switch.get(rand,0)
-
-class Color(enum.Enum):
-    RED = 1
-    GREEN = 2
-    BLUE = 3
-    PURPLE = 4
-
-class Country(enum.Enum):
-    ENGLAND = 1
-    FRANCE = 2
-    ARMENIA = 3
-
-
 
 class Soldier:
 
@@ -138,22 +183,22 @@ class Soldier:
     '''
 
     ''' Where does current soldier stand?'''
-    __location
+    __location = Location(0,0)
 
     ''' Which direction does he face?'''
-    __direction = NORTH
+    __direction = Direction.NORTH
 
     '''Number from [0..100] where 0 is dead.'''
     __health = 100
 
     '''How good is this soldier at attacking.'''
-    __attack
+    __attack = 0
 
     '''How good is this soldier at defending.'''
-    __defense
-	
+    __defense = 0
+    
     '''Solder knows to which player it belongs.'''
-    __player
+    __player = Player("", "", 0, Country.ENGLAND, Color.RED)
 
     def __init__(self, player, location, direction,\
          health, attack, defense):
@@ -216,37 +261,91 @@ class Soldier:
     '''returns True if everything went fine and false otherwise.'''
     def turn(self, new_direction):
         self.__direction = new_direction
-	
-	'''Turns the soldier in a random direction.'''
-	def turnRandom(self):
-        self.__direction = self.__direction.getRandomDirection()
+    
+    '''Turns the soldier in a random direction.'''
+    def turnRandom(self):
+        self.__direction = getRandomDirection()
 
     '''Moves soldier in the direction it faces now.'''
     def move(self):
         if self.__direction == Direction.NORTH:
-            self__location.setY(self.__location.getY() - 1)
-		elif self.__direction == Direction.SOUTH:
-            self__location.setY(self.__location.getY() + 1)
+            self.__location.setY(self.__location.getY() - 1)
+        elif self.__direction == Direction.SOUTH:
+            self.__location.setY(self.__location.getY() + 1)
         elif self.__direction == Direction.EAST:
-            self__location.setX(self.__location.getX() + 1)
+            self.__location.setX(self.__location.getX() + 1)
         elif self.__direction == Direction.WEST:
-            self__location.setX(self.__location.getX() - 1)
+            self.__location.setX(self.__location.getX() - 1)
         elif self.__direction == Direction.EASTNORTH:
-            self__location.setY(self.__location.getY() - 1)
-            self__location.setX(self.__location.getX() + 1)
+            self.__location.setY(self.__location.getY() - 1)
+            self.__location.setX(self.__location.getX() + 1)
         elif self.__direction == Direction.EASTSOUTH:
-            self__location.setY(self.__location.getY() + 1)
-            self__location.setX(self.__location.getX() + 1)
+            self.__location.setY(self.__location.getY() + 1)
+            self.__location.setX(self.__location.getX() + 1)
         elif self.__direction == Direction.WESTNORTH:
-            self__location.setY(self.__location.getY() - 1)
-            self__location.setX(self.__location.getX() - 1)
+            self.__location.setY(self.__location.getY() - 1)
+            self.__location.setX(self.__location.getX() - 1)
         elif self.__direction == Direction.WESTSOUTH:
-            self__location.setY(self.__location.getY() + 1)
-            self__location.setX(self.__location.getX() - 1)
+            self.__location.setY(self.__location.getY() + 1)
+            self.__location.setX(self.__location.getX() - 1)
         else:
             print "Invalid direction"
-	
 
-if __name__ == "__main__"
-    p1 = Player("Levon", 100, Armenia, RED)
-    p2 = Player("Raz", 100, ENGLAND, GREEN)
+    def printSoldier(self):
+
+        p = self.getPlayer()
+        p.printPlayer()
+
+        l = self.getLocation()
+        print "\nLocation = (",l.getX(), l.getY(),")"
+        
+        print "Direction =",self.getDirection()        
+        print "Health =",self.getHealth()
+        print "Attack =",self.getAttack()
+        print "Defense =",self.getDefense()
+        
+    
+class Swordsman(Soldier):
+
+    __radius = 2
+
+    def __init__(self, player, location, direction,\
+         health, attack, defense, radius):
+        Soldier.__init__(self,player, location, direction,\
+         health, attack, defense)
+        self.__radius = radius
+
+class Archer(Soldier):
+
+    __radius = 5
+
+    def __init__(self, player, location, direction,\
+         health, attack, defense, radius):
+        Soldier.__init__(self,player, location, direction,\
+         health, attack, defense)
+        self.__radius = radius
+
+class Mag(Soldier):
+
+    __radius = 6
+
+    def __init__(self, player, location, direction,\
+         health, attack, defense, radius):
+        Soldier.__init__(self,player, location, direction,\
+         health, attack, defense)
+        self.__radius = radius
+
+if __name__ == "__main__":
+
+    s1 = Soldier(Player("1234", "Levon", 100, Country.ARMENIA, Color.RED), \
+        Location(3,4), getRandomDirection(), 100, 150, 80)
+    s2 = Soldier(Player("1235", "Raz", 100, Country.ENGLAND, Color.GREEN), \
+        Location(5,12), getRandomDirection(), 100, 120, 90)
+
+    s1.printSoldier() 
+    s2.printSoldier()
+
+    sw = Swordsman(Player("1234", "Arthur", 100, Country.FRANCE, Color.BLUE), \
+        Location(10,54), getRandomDirection(), 199, 128, 88)
+    sw.printSoldier()
+    
