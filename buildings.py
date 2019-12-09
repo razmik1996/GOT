3991bf7775a3a11318203e185e08fd58337935d7
Your Name <you@example.com>
2019-12-09 19:41:05 +0400

    pushed

--- buildings.py
+++ buildings.py
@@ -74,24 +74,24 @@ class Buildings:
         self.__defense = defense
     
 class ArcherBuilding(Buildings):
-    def __init__(self, hp = 100, spawnTime = 7):
-        Buildings.__init__(self, hp, spawnTime)
+    def __init__(self, player, location, health, atack, spawnTime, defense):
+        Buildings.__init__(self, player, location, health, atack, spawnTime, defense)
  
     def __str__(self):
-        return "This is Archer house hp = " + str(self.__hp) + " Spawn time = " + str(self.__spawnTime)
+        return "This is Archer house hp = " + str(self.__health) + " Spawn time = " + str(self.__spawnTime)
  
 class SwordsmanBuilding(Buildings):
-    def __init__(self, hp = 100, spawnTime = 5):
-        Buildings.__init__(self, hp, spawnTime)
+    def __init__(self, player, location, health, atack, spawnTime, defense):
+        Buildings.__init__(self, player, location, health, atack, spawnTime, defense)
  
     def __str__(self):
-        return "This is Swiordsman house hp = " + str(self.__hp) + " Spawn time = " + str(self.__spawnTime)
+        return "This is Swiordsman house hp = " + str(self.__health) + " Spawn time = " + str(self.__spawnTime)
  
 class MagBuilding(Buildings):
-    def __init__(self, hp = 100, spawnTime = 10):
-        Buildings.__init__(self, hp, spawnTime)
+    def __init__(self, player, location, health, atack, spawnTime, defense):
+        Buildings.__init__(self, player, location, health, atack, spawnTime, defense)
  
     def __str__(self):
-        return "This is Mags house hp = " + str(self.__hp) + " Spawn time = " + str(self.__spawnTime)
+        return "This is Mags house hp = " + str(self.__health) + " Spawn time = " + str(self.__spawnTime)
 
 
