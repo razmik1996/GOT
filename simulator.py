5f52fdab8dbca7c1a6bb07f51e50053de28cff02
Your Name <you@example.com>
2019-12-12 20:31:19 +0400

    Gui

--- simulator.py
+++ simulator.py
@@ -1,23 +1,40 @@
+#!/usr/bin/env python
 from Tkinter import *
+from PIL import Image, ImageTk
+
+def exitfromgame():
+    exit()
 
 root = Tk()
 
-topFrame = Frame(root)
-topFrame.pack()
+ws = root.winfo_screenwidth()
+hs = root.winfo_screenheight()
 
-botFrame = Frame(root)
-botFrame.pack(side = BOTTOM)
+root.attributes("-fullscreen", True)
+canvas = Canvas(root)
+root.title("Game Of Thrones")
+canvas.pack(expand = YES, fill = BOTH)
 
-Button1 = Button(topFrame, text = "Start",fg = "Red")
-Button1.pack(side=LEFT)
+frame = Frame(root, bg = None)
 
-Button2 = Button(topFrame, text = "Load",fg = "Blue")
-Button2.pack(side=LEFT)
+backgroundImage = Image.open("Sprites/Background.png")
+backgroundImage = backgroundImage.resize((ws, hs))
+backgroundImage = ImageTk.PhotoImage(backgroundImage)
+canvas.create_image(0, 0, image = backgroundImage, anchor = NW)
+canvas.create_window(ws/2 + 250, hs/2, window = frame, width = 300, height = 800)
 
-Button3 = Button(botFrame, text = "Settings",fg = "Green")
-Button3.pack(side=LEFT)
 
-Button4 = Button(botFrame, text = "Quit",fg = "Black")
-Button4.pack(side=LEFT)
+Button1 = Button(canvas, text = "Start",fg = "Red")
+Button1.pack(ipadx = 50, ipady = 50, padx = 300, pady = 100)
+
+Button2 = Button(canvas, text = "Load",fg = "Blue")
+Button2.pack(ipadx = 50, ipady = 50, padx = 360, pady = 160)
+
+Button3 = Button(canvas, text = "Settings",fg = "Green")
+Button3.pack(ipadx = 50, ipady = 50, padx = 380, pady = 220)
+
+Button4 = Button(canvas, text = "Quit",fg = "Black", command = exitfromgame)
+Button4.pack(padx = 1, pady = 1)
+
 
 root.mainloop()
