fd30663468dde44865abe5004e21fda1e15ab883
razmik1996 <razfreelancer@mail.ru>
2019-12-13 12:25:22 +0400

    adaptive

--- simulator.py
+++ simulator.py
@@ -10,6 +10,8 @@ root = Tk()
 ws = root.winfo_screenwidth()
 hs = root.winfo_screenheight()
 
+print ws, hs
+
 root.attributes("-fullscreen", True)
 canvas = Canvas(root)
 root.title("Game Of Thrones")
@@ -21,19 +23,19 @@ backgroundImage = Image.open("Sprites/Background.png")
 backgroundImage = backgroundImage.resize((ws, hs))
 backgroundImage = ImageTk.PhotoImage(backgroundImage)
 canvas.create_image(0, 0, image = backgroundImage, anchor = NW)
-canvas.create_window(ws/2 + 250, hs/2, window = frame, width = 300, height = 800)
+canvas.create_window(ws/2 + ws/6, hs/2, window = frame, width = ws/6, height = 5*hs/6)
 
 
-Button1 = Button(canvas, text = "Start",fg = "Red")
+Button1 = Button(frame, text = "Start",fg = "Red")
 Button1.pack(ipadx = 50, ipady = 50, padx = 300, pady = 100)
 
-Button2 = Button(canvas, text = "Load",fg = "Blue")
+Button2 = Button(frame, text = "Load",fg = "Blue")
 Button2.pack(ipadx = 50, ipady = 50, padx = 360, pady = 160)
 
-Button3 = Button(canvas, text = "Settings",fg = "Green")
+Button3 = Button(frame, text = "Settings",fg = "Green")
 Button3.pack(ipadx = 50, ipady = 50, padx = 380, pady = 220)
 
-Button4 = Button(canvas, text = "Quit",fg = "Black", command = exitfromgame)
+Button4 = Button(frame, text = "Quit",fg = "Black", command = exitfromgame)
 Button4.pack(padx = 1, pady = 1)
 
 
